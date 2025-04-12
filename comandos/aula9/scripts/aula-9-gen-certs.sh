f20f9e9517aa2f21f13d4ebacc883a5514f9476c
leolm83 <leonardo.mateus01@hotmai.com>
2025-04-12 12:17:40 -0300

    aula 9 - bash script melhorado

--- comandos/aula9/scripts/aula-9-gen-certs.sh
+++ comandos/aula9/scripts/aula-9-gen-certs.sh
@@ -1,50 +1,118 @@
 #!/bin/bash
+#general vars
 password='@DevOpsNaNuvem$%!'
 days=365
 keySize=4096
 
-openssl genrsa \
-    -des3 \
-    -passout pass:$password \
-    -out ../certificates/root-ca.key \
-    $keySize 
-## gerar certificado:
-openssl req \
-    -x509 \
-    -new \
-    -key ../certificates/root-ca.key \
-    -passin pass:$password \
-    -days $days \
-    -sha256 \
-    -out ../certificates/root-ca.crt \
-    -subj '/CN=devopsnanuvem.internal'
+#rootCA vars
+rootCAKey='../certificates/root-ca.key'
+rootCACRT='../certificates/root-ca.crt'
 
+#signingRequest vars
+signingRequestKey='../certificates/signing-request.key'
+signingRequestCSR='../certificates/signing-request.csr' 
+
+
+extFile='../certificates/config.ext'
+nginxCertificate='../certificates/nginx-certificate.crt'
+
+kestrelCertificate='../certificates/kestrel-certificate.pfx'
+
+
+function generateCACertificate(){
+    feedbackMessage='Certificate Authority Certificate'
+    
+
+    if [ -f  $rootCAKey ]
+    then
+        echo "$feedbackMessage Already Exists! in $rootCAKey" 
+    else
+        echo "$feedbackMessage Don't Exists!" 
+        echo "Generating CA certificate"
+        openssl genrsa \
+            -des3 \
+            -passout pass:$password \
+            -out $rootCAKey \
+            $keySize 
+    fi
+
+    if [ -f $rootCAKey ] && [ ! -f $rootCACRT ]
+    then
+        echo "Generating Root CA Certificate"
+        openssl req \
+            -x509 \
+            -new \
+            -key $rootCAKey \
+            -passin pass:$password \
+            -days $days \
+            -sha256 \
+            -out $rootCACRT \
+            -subj '/CN=devopsnanuvem.internal'
+    else
+        echo 'Root CA Certificate already exists or Root CA Key doesnt exists'
+    fi
+}
+
+function generateCertificateSigningRequest(){
+
+    if [ ! -f $signingRequestKey ] && [ ! -f $signingRequestCSR ]
+    then
+        echo 'Generating Signing Request Key and Signing Request CSR'
+        openssl req \
+        -new \
+        -noenc \
+        -newkey rsa:$keySize \
+        -keyout $signingRequestKey \
+        -out $signingRequestCSR \
+        -subj '/CN=devopsnanuvem.internal'
+    else
+        echo 'Generating Signing Request Key or Signing Request CSR already exists'
+    fi
+}
+
+function generateNginxCertificate(){
+    if [ -f $extFile ] && [ -f $signingRequestCSR ] && [ -f $rootCACRT ] && [ -f $rootCAKey ] && [ ! -f $nginxCertificate ]
+    then
+        echo 'Generating Nginx x509 certificate'
+        openssl x509 \
+        -req \
+        -in $signingRequestCSR \
+        -CA $rootCACRT \
+        -CAkey $rootCAKey \
+        -passin pass:$password \
+        -sha256 \
+        -CAcreateserial \
+        -days $days \
+        -out $nginxCertificate \
+        -extfile $extFile
+    else
+        echo 'Some of the needed files doesnot exist or the Nginx Cert already exists'
+    fi
+}
+
+function generateKestrelCertificate(){
+   
+    if [ -f $signingRequestKey ] && [ -f $nginxCertificate ] && [ ! -f $kestrelCertificate ]
+    then 
+        openssl pkcs12 \
+        -inkey $signingRequestKey \
+        -in $nginxCertificate \
+        -export \
+        -out $kestrelCertificate \
+        -passout pass:$password
+    else
+        echo 'Algum dos arquivos necessarios nao existe ou o cerfiticado do Kestrel ja foi gerado anteriormente'
+    fi
+}
+
+generateCACertificate;
 ## (CSR) Certificate Signing Request - Solicitacao de assinatura de certificado
-openssl req \
-    -new \
-    -noenc \
-    -newkey rsa:$keySize \
-    -keyout ../certificates/signing-request.key \
-    -out ../certificates/signing-request.csr \
-    -subj '/CN=devopsnanuvem.internal'
-
+generateCertificateSigningRequest;
 ## CERTIFICADO PARA O NGINX
-openssl x509 \
-    -req \
-    -in ../certificates/signing-request.csr \
-    -CA ../certificates/root-ca.crt \
-    -CAkey ../certificates/root-ca.key \
-    -passin pass:$password \
-    -sha256 \
-    -CAcreateserial \
-    -days $days \
-    -out ../certificates/nginx-certificate.crt \
-    -extfile ../certificates/config.ext
+generateNginxCertificate;
+
+generateKestrelCertificate;
+
 
 ## CERTIFICADO PARA O KESTREL
-openssl pkcs12 \
-    -inkey ../certificates/signing-request.key \
-    -in ../certificates/nginx-certificate.crt \
-    -export \
-    -out ../certificates/kestrel-certificate.pfx \
-    -passout pass:$password
+
